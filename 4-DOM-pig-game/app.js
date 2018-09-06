068de47b4cc8ce233da4a56ef896f8975f983f98
poojakancherla <pknq6@mst.edu>
2018-09-05 23:15:41 -0500

    Implemented the 'HOLD' function and also DRY principle

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -42,21 +42,49 @@ document.querySelector('.btn-roll').addEventListener('click', function()
     else
     {
         // Next Player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-
-        document.getElementById('current-0').textContent = '0'
-        document.getElementById('current-1').textContent = '0'
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        document.querySelector('.dice').style.display = 'none';
-
+        
+        nextPlayer();
     }
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function()
+{
+    // Add current score to global score
+    scores[activePlayer] += roundScore;    
 
+    // Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    // Check if player won the game  
+    if (scores[activePlayer] >= 20)
+    {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    }
+    else
+    {
+        // Next Player
+    nextPlayer();
+    }    
+
+});
+
+function nextPlayer()
+{    
+        
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 
