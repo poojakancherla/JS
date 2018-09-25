38c684b702202b58045df5c772c4a974e3a4e7a4
poojakancherla <pknq6@mst.edu>
2018-09-25 06:56:02 -0500

    Added the new item to the UI

--- Budgety/app.js
+++ Budgety/app.js
@@ -63,7 +63,9 @@ var UIController = (function()
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     };
     return{
         getInput: function()
@@ -74,6 +76,32 @@ var UIController = (function()
             value : document.querySelector('.add__value').value
             };
         },
+
+        addItemList: function(obj, type)
+        {
+            var html ,element, newHtml; // element here is for the html containers where we display items 
+            // Get the HTML
+            if(type === 'inc')
+            {
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+            else if(type === 'exp')
+            {
+                element = DOMstrings.expensesContainer;
+                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+
+            // Replace place holders in html with data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            // Insert the new html into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+            
+        },
+
         getDOMstrings: function()
         {
             return DOMstrings;
@@ -108,6 +136,7 @@ var controller = (function(budgetCtrl, UICtrl)
         // 2. Add the item to the budget controller
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3. Add the item to the UI
+        UICtrl.addItemList(newItem, input.type);
         // 4. Calculate the budget
         // 5. Display the budget on the UI
     };
